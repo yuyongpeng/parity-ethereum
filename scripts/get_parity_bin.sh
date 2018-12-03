d46b4e6f4c23095b7cae87bf0e66a845481f95e4
juniuszhou <junius.zhou@gmail.com>
2018-12-03 09:57:33 +0800

    add configure file and script for deploy hard chain.

---
+++ scripts/get_parity_bin.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+scp ubuntu@106.75.19.197://data/parity/parity-ethereum/target/armv7-unknown-linux-gnueabihf/release/parity /mnt/parity/parity-ethereum/target/release/
+
---
+++ scripts/start_parity.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+/data/parity-ethereum/target/release/parity --jsonrpc-interface 0.0.0.0 --base-path /data/parity_data --chain /data/parity-ethereum/ethcore/res/ethereum/hardchain.json -l main=trace,network=trace,sync=trace,spec=trace
+
+# /mnt/parity/parity-ethereum/target/release/
