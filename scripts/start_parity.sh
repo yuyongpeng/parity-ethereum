4cb5021bd0b1a57e37c2987fae34a41813cbb69b
juniuszhou <junius.zhou@gmail.com>
2018-12-03 10:24:22 +0800

    add configure file and script for deploy hard chain.

--- scripts/start_parity.sh
+++ scripts/start_parity.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-/data/parity/parity-ethereum/target/release/parity --jsonrpc-interface 0.0.0.0 --base-path /data/parity/parity_data --chain /data/parity-ethereum/ethcore/res/ethereum/hardchain.json -l main=trace,network=trace,sync=trace,spec=trace
+/data/parity/parity-ethereum/target/release/parity --jsonrpc-interface 0.0.0.0 --base-path /data/parity/parity_data --chain /data/parity/parity-ethereum/ethcore/res/ethereum/hardchain.json -l main=trace,network=trace,sync=trace,spec=trace
 
 # path for dphotos.
 # /mnt/parity/parity-ethereum/target/release/
