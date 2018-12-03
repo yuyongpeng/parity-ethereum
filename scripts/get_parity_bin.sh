e634da437b4fe47d0766f65b402cf0579536b120
juniuszhou <junius.zhou@gmail.com>
2018-12-03 10:18:41 +0800

    add configure file and script for deploy hard chain.

--- scripts/get_parity_bin.sh
+++ scripts/get_parity_bin.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+
+# copy arm bin from compiler host.
 scp ubuntu@106.75.19.197://data/parity/parity-ethereum/target/armv7-unknown-linux-gnueabihf/release/parity /mnt/parity/parity-ethereum/target/release/
 
