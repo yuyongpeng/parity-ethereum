161f8d2313076fc701f407ce89fcd50c9a8b3a40
juniuszhou <junius.zhou@gmail.com>
2018-12-21 14:30:00 +0800

    update get bin.

--- scripts/get_parity_bin.sh
+++ scripts/get_parity_bin.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-# copy arm bin from compiler host.
+# copy arm bin from compiler host. ethEr)!0x
 mkdir -p /mnt/dphotos/parity/parity-ethereum/target/release/
 scp ubuntu@106.75.19.197://data/parity/parity-ethereum/target/armv7-unknown-linux-gnueabihf/release/parity /mnt/dphotos/parity/parity-ethereum/target/release/
+
+scp ubuntu@106.75.19.197://data/parity/ethminer/build/ethminer/ethminer /data/parity/
 
