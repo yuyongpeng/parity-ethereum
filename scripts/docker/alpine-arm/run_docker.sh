e8437ac0d3314b26a83c52a2f74a615221fba94d
juniuszhou <junius.zhou@gmail.com>
2019-01-07 13:50:05 +0800

    update configure file path for device.

--- scripts/docker/alpine-arm/run_docker.sh
+++ scripts/docker/alpine-arm/run_docker.sh
@@ -1,3 +1,4 @@
-docker run -ti -v /root/start_parity.sh:/root/start_parity.sh -v /root/log.txt:/root/log.txt \
--v /root/hardchain.toml:/root/hardchain.toml -v /root/hardchain.json:/root/hardchain.json \
+docker run -d -v /mnt/dphotos/parity/start_parity.sh:/root/start_parity.sh \
+-v /mnt/dphotos/parity/hardchain.toml:/root/hardchain.toml -v /mnt/dphotos/parity/hardchain.json:/root/hardchain.json \
+-v /mnt/dphotos/parity/chain_data:/root/chain_data --network host --restart=always \
 uhub.service.ucloud.cn/hard_docker/parity-arm:latest
--- scripts/docker/alpine-arm/start_parity.sh
+++ scripts/docker/alpine-arm/start_parity.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
-
-/root/parity --light --warp --jsonrpc-interface 0.0.0.0 --base-path /root/chain_data --config /root/hardchain.toml --chain /root/hardchain.json -l main=trace > /root/log.txt 2>&1 &
+/bin/sh
+/root/parity --light --warp --jsonrpc-interface 0.0.0.0 --base-path /root/chain_data --config /root/hardchain.toml --chain /root/hardchain.json -l main=trace
 
