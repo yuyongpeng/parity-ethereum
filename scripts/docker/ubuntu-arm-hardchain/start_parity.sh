064444e20668001bfbb4ca65a4e3eb54f3ef83a4
juniuszhou <junius.zhou@gmail.com>
2019-03-07 17:32:02 +0800

    get a runable ubuntu arm image.

--- scripts/docker/ubuntu-arm-hardchain/start_parity.sh
+++ scripts/docker/ubuntu-arm-hardchain/start_parity.sh
@@ -1,3 +1,3 @@
-/bin/sh
+#!/bin/sh
 /root/parity --light --warp --jsonrpc-interface 0.0.0.0 --base-path /root/chain_data --config /root/hardchain.toml --chain /root/hardchain.json -l main=trace
 
--- scripts/get_parity_bin.sh
+++ scripts/get_parity_bin.sh
@@ -2,7 +2,7 @@
 
 # copy arm bin from compiler host. ethEr)!0x
 mkdir -p /mnt/dphotos/parity/parity-ethereum/target/release/
-scp ubuntu@106.75.19.197://data/parity/parity-ethereum/target/armv7-unknown-linux-gnueabihf/release/parity /mnt/dphotos/parity/parity-ethereum/target/release/
+scp -P 2345 ubuntu@106.75.19.197://data/parity/parity-ethereum/target/armv7-unknown-linux-gnueabihf/release/parity /mnt/dphotos/parity/parity-ethereum/target/release/
 
-scp ubuntu@106.75.19.197://data/parity/ethminer/build/ethminer/ethminer /data/parity/
+scp -P ubuntu@106.75.19.197://data/parity/ethminer/build/ethminer/ethminer /data/parity/
 
